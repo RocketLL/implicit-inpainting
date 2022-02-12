4305b8237122cda7a5298a72f7a75cbcc4f2f869
Lucas Yunkyu Lee <me@luc.li>
2022-02-12 17:34:58 +0900

    implement minibatching for iif

--- train_iif.py
+++ train_iif.py
@@ -7,6 +7,9 @@ from utils.image import *
 from utils.io import *
 from modules.conv2d_resblock import Conv2DResBlock
 
+BATCH_SIZE = 16
+W = H = 32
+
 
 class IIF(nn.Module):
     def __init__(self, dims=3, channels=128, blocks=8):
@@ -24,7 +27,7 @@ class IIF(nn.Module):
         )
 
         self.decoder = nn.Sequential(
-            nn.Linear(2 + 32 * 32 * 3, 256),
+            nn.Linear(2 + W * H * 3, 256),
             nn.ReLU(),
             nn.Linear(256, 256),
             nn.ReLU(),
@@ -44,31 +47,53 @@ class IIF(nn.Module):
         return z
 
     def forward(self, image, mask, x):
-        Z = self.encoder(image.unsqueeze(0))
+        # image: (BATCH_SIZE, 3, W, H)
+        # mask:  (BATCH_SIZE, 3, W, H)
+        # x:     (BATCH_SIZE, n, 2)
+
+        Z = self.encoder(image)
         Z *= mask
         z = self._sample_latent(Z, x)
-        # Z: 3, 32, 32
-        # x: n, 2
 
-        z = z.flatten()
-        # z: 3072
+        # Z: (BATCH_SIZE, 3, W, H)
+        # x: (BATCH_SIZE, n, 2)
 
-        decoder_input = torch.hstack((x, z.unsqueeze(0).repeat(x.shape[0], 1)))
+        z = z.flatten(1)
+        # z: (BATCH_SIZE, 3 * W * H)
+
+        decoder_input = torch.cat((x, z.unsqueeze(1).repeat(1, x.shape[-2], 1)), -1)
         s = self.decoder(decoder_input)
 
         return s
 
 
+def to_coords(image: torch.Tensor, mask: torch.Tensor):
+    # image: (BATCH_SIZE, 3, W, H)
+    # mask: (BATCH_SIZE, 3, W, H)
+    coords = mask[0, 0].nonzero(as_tuple=True)
+    image_data = image[..., :, coords[0], coords[1]].transpose(-1, -2)
+
+    coords = mask[0, 0].nonzero().unsqueeze(0).repeat(image_data.shape[0], 1, 1)
+
+    return (image_data, coords)
+
+
 def from_coords(shape, x, s):
+    # x: (BATCH_SIZE, n, 2)
+    # s: (BATCH_SIZE, n, 3)
     image = torch.zeros(shape, device=x.device)
-    image[:, x.T[0], x.T[1]] = s.T
+    # image: (BATCH_SIZE, n, W, H)
+
+    for i in range(shape[0]):
+        image[i, :, x.T[0], x.T[1]] = s.T
 
     return image
 
 
-def to_coords(image: torch.Tensor, mask: torch.Tensor):
-    coords = mask[0].nonzero(as_tuple=True)
-    return image[:, coords[0], coords[1]].T, mask[0].nonzero()
+def normalize_coords(x):
+    x[..., 0] = x[..., 0] / (W - 1) * 2 - 1.0
+    x[..., 1] = x[..., 1] / (H - 1) * 2 - 1.0
+    return x
 
 
 def train(model, dataloader, optimizer, device):
@@ -76,13 +101,15 @@ def train(model, dataloader, optimizer, device):
     netloss = 0
 
     for i, (images, _) in enumerate(dataloader):
-        images = images.to(device)[0]
-        masked, mask = mask_random(images, 0.5)
-        
-        s, x = to_coords(images, mask.logical_not())
+        images = images.to(device)
+        masked, mask = mask_random(
+            images, torch.rand(1) * 0.8 + 0.1
+        )  # uniform(0.1, 0.9)
 
-        # s: (n, 3)
-        # x: (n, 2)
+        s, x = to_coords(images, mask.logical_not())
+        x = normalize_coords(x)
+        # s: (BATCH_SIZE, n, 3)
+        # x: (BATCH_SIZE, n, 2)
 
         optimizer.zero_grad()
         s_pred = model(masked, mask, x)
@@ -101,13 +128,15 @@ def train(model, dataloader, optimizer, device):
 
 def validate(model, dataiter, device):
     model.eval()
+
     with torch.no_grad():
         images, _ = next(dataiter)
-        images = images.to(device)[0]
+        images = images.to(device)
 
         masked, mask = mask_random(images, 0.5)
-        
+
         s, x = to_coords(images, mask.logical_not())
+        x = normalize_coords(x)
 
         s_pred = model(masked, mask, x)
         loss = ((s - s_pred) ** 2).mean()
@@ -116,7 +145,7 @@ def validate(model, dataiter, device):
 
 
 if __name__ == "__main__":
-    load_prev = True
+    load_prev = False
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     checkpoint_path = "iif.tar"
 
@@ -125,12 +154,12 @@ if __name__ == "__main__":
         "train",
         download=False,
         transform=transforms.Compose(
-            [transforms.ToTensor(), transforms.Resize((32, 32))]
+            [transforms.ToTensor(), transforms.Resize((W, H))]
         ),
     )
     trainloader = DataLoader(
         trainset,
-        1,
+        BATCH_SIZE,
         shuffle=True,
     )
 
@@ -139,10 +168,10 @@ if __name__ == "__main__":
         "valid",
         download=False,
         transform=transforms.Compose(
-            [transforms.ToTensor(), transforms.Resize((32, 32))]
+            [transforms.ToTensor(), transforms.Resize((W, H))]
         ),
     )
-    validloader = DataLoader(validset, 1, shuffle=True)
+    validloader = DataLoader(validset, BATCH_SIZE, shuffle=True)
 
     model = IIF().to(device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
